cd981b37f6f75e90e3358892d5e10f79e9aa4dab
Jason W <jasonwon628@gmail.com>
2015-04-08 15:38:34 -0400

    init

---
+++ dialog.js
@@ -0,0 +1,25 @@
+var custom_alert = function(text) {
+    var d = document;
+    var shadow = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
+    shadow.className = "shadow";
+    var lr = shadow.appendChild(d.createElement("div"));
+    lr.className = "left-in";
+    var rl = shadow.appendChild(d.createElement("div"));
+    rl.className = "right-in";
+    var shield = shadow.appendChild(d.createElement("div"));
+    shield.className = "shield";
+    var alert = shield.appendChild(d.createElement("div"));
+    alert.className = "alert";
+    alert.innerHTML = text;
+    var close = shield.appendChild(d.createElement("button"));
+    close.className = "close";
+    close.innerHTML = "close"
+    close.onclick = function() {
+        lr.className = "left-out";
+        rl.className = "right-out";
+        setTimeout(function() {
+            shadow.parentNode.removeChild(shadow);
+        },1000);
+    }
+
+}
